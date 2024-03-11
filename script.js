b16443575a438cfd7e3c0d7210eda50adb8d20bd
Rogier Goossen <92125322+rogierok@users.noreply.github.com>
2024-03-11 11:46:08 +0100

    Added re roll button and fixed mobile

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 let gift = document.getElementById("gift")
 let item = document.getElementById("item")
-gift.addEventListener("click", opening,{once:true})
+let again = document.getElementById("again")
 
-var items = ["🍩", "🍓", "🍔", "🍔", "🍔", "🍪", "🥪", "🍕", "🧇", "🍫", "🍭", "🍬", "🧃", "🍌", "🍉", "🍇", "🍎", "🥑"]
+gift.addEventListener("click", opening)
+
+again.addEventListener("click", reset)
+
+var items = ["🍩", "🍓", "🍔", "🍔", "🍔", "🍪", "🥪", "🍕", "🧇", "🍫", "🍭", "🍬", "🧃", "🍌", "🍉", "🍇", "🍎", "🥑", "🥦", "🥒", "🥒"]
 
 function opening() {
-    console.log("yooo")
-    gift.style.opacity= 0;
+    again.style.scale = 1;
+    gift.style.pointerEvents = "none";
+    gift.style.opacity = 0;
 
     var randomitem = items[Math.floor(Math.random() * items.length)];
 
     item.innerHTML = randomitem
     item.style.scale= 1;
     item.style.opacity= 1;
+
+    again.style.pointerEvents = "all";
+    again.style.opacity = 1;
+}
+
+function reset () {
+    gift.style.pointerEvents = "all";
+    gift.style.opacity = 1;
+
+    item.style.scale= 0;
+    item.style.opacity= 0;
+
+    again.style.pointerEvents = "none";
+    again.style.opacity = 0;
+    again.style.scale = 0;
 }
