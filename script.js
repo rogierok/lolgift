46bc2b04bdf71462b43609cf7193daa5bcbb72ec
Rogier Goossen <92125322+rogierok@users.noreply.github.com>
2024-03-22 11:30:08 +0100

    Cleaned up

--- script.js
+++ script.js
@@ -7,57 +7,55 @@ gift.addEventListener("click", opening)
 
 again.addEventListener("click", reset)
 
-var ods = [items, rareitems, mythicitems]
-
 var trashitems = ["🫘", "🥫", "🥦", "🥒"]
 var items = ["🍓", "🥪", "🧃", "🍌", "🍉", "🍇", "🍎", "🥑", "🍊", "🍋", "🍍", "🍐", "🍒", "🥝", "🥥"]
 var rareitems = ["🍩", "🍔", "🧇", "🍫", "🍭", "🍬", "🌮"]
 var mythicitems = ["🥐", "🧀", "🍕", "🍪"]
 
 function opening() {
-    again.style.scale = 1;
-    gift.style.pointerEvents = "none";
-    gift.style.opacity = 0;
+    again.style.scale = 1
+    gift.style.pointerEvents = "none"
+    gift.style.opacity = 0
 
     var weight = Math.floor(Math.random() * 999)
 
     if (weight >= 850 &&  weight <= 950) {
         randomarray = rareitems
-        background.style.backgroundColor = "#567BFF";
+        background.style.backgroundColor = "#567BFF"
     }
     else if (weight > 950 &&  weight <= 999) {
         randomarray = mythicitems
-        background.style.backgroundColor = "#E11DE2";
+        background.style.backgroundColor = "#E11DE2"
     }
     else if (weight >= 0 &&  weight <= 150) {
         randomarray = trashitems
-        background.style.backgroundColor = "#603400";
+        background.style.backgroundColor = "#603400"
     }
     else {
         randomarray = items
-        background.style.backgroundColor = "#D1EFFF";
+        background.style.backgroundColor = "#D1EFFF"
     }
 
-    var randomitem = randomarray[Math.floor(Math.random() * randomarray.length)];
+    var randomitem = randomarray[Math.floor(Math.random() * randomarray.length)]
 
     item.innerHTML = randomitem
-    item.style.scale= 1;
-    item.style.opacity= 1;
+    item.style.scale= 1
+    item.style.opacity= 1
 
-    again.style.pointerEvents = "all";
-    again.style.opacity = 1;
+    again.style.pointerEvents = "all"
+    again.style.opacity = 1
 }
 
 function reset () {
-    gift.style.pointerEvents = "all";
-    gift.style.opacity = 1;
+    gift.style.pointerEvents = "all"
+    gift.style.opacity = 1
 
-    item.style.scale= 0;
-    item.style.opacity= 0;
+    item.style.scale= 0
+    item.style.opacity= 0
 
-    again.style.pointerEvents = "none";
-    again.style.opacity = 0;
-    again.style.scale = 0;
+    again.style.pointerEvents = "none"
+    again.style.opacity = 0
+    again.style.scale = 0
 
-    background.style.backgroundColor = "#FFFFFF";
+    background.style.backgroundColor = "#FFFFFF"
 }
