b24bda02a81b0aaa20e242a1f47601cdd6a6b9bc
Rogier Goossen <92125322+rogierok@users.noreply.github.com>
2024-03-22 10:27:32 +0100

    Added rarities

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 let gift = document.getElementById("gift")
 let item = document.getElementById("item")
 let again = document.getElementById("again")
+let background = document.getElementById("background")
 
 gift.addEventListener("click", opening)
 
 again.addEventListener("click", reset)
 
-var items = ["🍩", "🍓", "🍔", "🍔", "🍔", "🍪", "🥪", "🍕", "🧇", "🍫", "🍭", "🍬", "🧃", "🍌", "🍉", "🍇", "🍎", "🥑", "🥦", "🥒", "🥒"]
+var ods = [items, rareitems, mythicitems]
+
+var trashitems = ["🫘", "🥫", "🥦", "🥒"]
+var items = ["🍓", "🥪", "🧃", "🍌", "🍉", "🍇", "🍎", "🥑", "🍊", "🍋", "🍍", "🍐", "🍒", "🥝", "🥥"]
+var rareitems = ["🍩", "🍔", "🧇", "🍫", "🍭", "🍬", "🌮"]
+var mythicitems = ["🥐", "🧀", "🍕", "🍪"]
 
 function opening() {
     again.style.scale = 1;
     gift.style.pointerEvents = "none";
     gift.style.opacity = 0;
 
-    var randomitem = items[Math.floor(Math.random() * items.length)];
+    var weight = Math.floor(Math.random() * 999)
+
+    if (weight >= 850 &&  weight <= 950) {
+        randomarray = rareitems
+        background.style.backgroundColor = "#567BFF";
+    }
+    else if (weight > 950 &&  weight <= 999) {
+        randomarray = mythicitems
+        background.style.backgroundColor = "#E11DE2";
+    }
+    else if (weight >= 0 &&  weight <= 150) {
+        randomarray = trashitems
+        background.style.backgroundColor = "#603400";
+    }
+    else {
+        randomarray = items
+        background.style.backgroundColor = "#D1EFFF";
+    }
+
+    var randomitem = randomarray[Math.floor(Math.random() * randomarray.length)];
 
     item.innerHTML = randomitem
     item.style.scale= 1;
@@ -33,4 +58,6 @@ function reset () {
     again.style.pointerEvents = "none";
     again.style.opacity = 0;
     again.style.scale = 0;
+
+    background.style.backgroundColor = "#FFFFFF";
 }
